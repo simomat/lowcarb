99e0daf4347994af850bf1c977b9a2bf2102f5b3
Matthias Simon <simon@infonautika.de>
2017-04-20 19:21:09 +0000

    refac

--- src/popup/domaincompare.js
+++ src/popup/domaincompare.js
@@ -1,35 +1,35 @@
 
-function* reversedParts(domain) {
-    let lastEnd = domain.length;
-    for (let index = lastEnd-1; index >= -1; --index) {
-        if (domain[index] == '.' || index == -1) {
-            yield domain.substring(index + 1, lastEnd);
-            lastEnd = index;
+function* reverseIterateParts(domain) {
+    let currentEnd = domain.length;
+    for (let index = currentEnd-1; index >= -1; index--) {
+        if (index == -1 || domain[index] == '.') {
+            yield domain.substring(index + 1, currentEnd);
+            currentEnd = index;
         }
     }
 }
 
 export function domainCompare(domainA, domainB) {
-    let partsA = reversedParts(domainA);
-    let partsB = reversedParts(domainB);
+    let partsOfA = reverseIterateParts(domainA);
+    let partsOfB = reverseIterateParts(domainB);
 
     while (true) {
-        let nextA = partsA.next();
-        let nextB = partsB.next();
+        let partA = partsOfA.next();
+        let partB = partsOfB.next();
 
-        if (nextA.done) {
-            if (nextB.done) {
+        if (partA.done) {
+            if (partB.done) {
                 return 0;
             }
             return -1;
         }
-        if (nextB.done) {
+        if (partB.done) {
             return 1;
         }
-        if (nextA.value > nextB.value) {
+        if (partA.value > partB.value) {
             return 1;
         }
-        if (nextA.value < nextB.value) {
+        if (partA.value < partB.value) {
             return -1;
         }
     }
