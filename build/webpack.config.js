71518eb2a88443e039ce3f6ecab2b629aeebc7d3
Matthias Simon <simon@infonautika.de>
2021-11-30 06:13:11 +0100

    vue works

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { VueLoaderPlugin } = require("vue-loader");
 
 const build_dir = __dirname + '/dist/'
 const template_dir = __dirname + '/../extension/'
@@ -8,20 +9,42 @@ module.exports = {
   entry: './src/background.js',
   entry: {
     'background': './src/background.js',
-    'options': './src/options/options.js'
+    'options': './src/options/main.js'
   },
   output: {
     path: build_dir,
     filename: "[name].js",
     clean: true
   },
+  module: {
+    rules: [
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: "babel-loader",
+      //   },
+      // },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+    ],
+  },
   plugins: [
     new CopyWebpackPlugin({
       patterns: [
         { from: template_dir, to: build_dir }
       ]
-    })
+    }),
+    new VueLoaderPlugin(),
   ],
+  // resolve: {
+  //   alias: {
+  //     vue$: "vue/dist/vue.runtime.esm.js",
+  //   },
+  //   extensions: ["*", ".js", ".vue", ".json"],
+  // },
   optimization: {
     minimize: false,
   }
---
+++ src/options/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>
+      Dingsii {{ dings }}
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            dings: 'halloooo'
+        }
+        
+    }
+}
+</script>
